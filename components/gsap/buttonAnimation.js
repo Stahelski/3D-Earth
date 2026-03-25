c7722775afdbcb7665d534b5e126ed00b56f6348
Stian Karlsen <stiankarlsen709@gmail.com>
2026-03-25 23:58:32 +0100

    path yesss

--- components/gsap/buttonAnimation.js
+++ components/gsap/buttonAnimation.js
@@ -7,15 +7,8 @@ gsap.registerPlugin(MotionPathPlugin);
 export default function btnAnimation(btn, txt) {
   if (!btn || !txt) return;
 
-  const pathElement = document.getElementById("sti");
-  if (!pathElement) {
-    console.warn("SVG path med id='sti' ble ikke funnet");
-  }
-
   // Lag en timeline for click-animasjonen (paused, start ved klikk)
   const tl = gsap.timeline({ paused: true });
-  const svg = document.getElementById("#svg");
-  const path = document.getElementById("#sti");
 
   tl.to(btn, {
     rotation: 720,
@@ -24,48 +17,58 @@ export default function btnAnimation(btn, txt) {
     borderRadius: "100%",
     duration: 1.8,
     ease: "power2.inOut",
-  })
-    .to(
-      txt,
-      {
-        color: "transparent",
-        duration: 0.4,
-      },
-      0,
-    )
-    .to(btn, {
-      x: -40,
-      y: 5,
-      duration: 1,
-      ease: "elastic.out(1, 0.5)",
-    });
+  }).to(
+    txt,
+    {
+      color: "transparent",
+      duration: 0.4,
+    },
+    0,
+  );
 
-  tl.set(svg, { opacity: 0 });
-  tl.from(path, { drawSVG: "0% 0%" });
-  tl.to(path, { drawSVG: "100% 100%" });
+  tl.to(btn, {
+    x: -40,
+    y: 5,
+    duration: 1,
+    ease: "elastic.out",
+  });
+  tl.to(btn, {
+    x: 80,
+    y: -70,
+    ease: "elastic.out",
+  });
+  tl.to(btn, {
+    duration: 2,
+    motionPath: {
+      path: "M0,0 C100,40 200,60 300,100",
+      align: "self",
+      alignOrigin: [0.5, 0.5],
+    },
+    ease: "elastic.out",
+  });
 
-  const resetAnimation = () => {
-    gsap.to(btn, {
-      rotation: 0,
-      width: "180px",
-      height: "40px",
-      borderRadius: "11px",
-      fontSize: "1.35rem",
-      duration: 1.4,
-      ease: "power2.inOut",
-    });
+  // const resetAnimation = () => {
+  //   gsap.to(btn, {
+  //     rotation: 0,
+  //     width: "180px",
+  //     height: "40px",
+  //     borderRadius: "11px",
+  //     fontSize: "1.35rem",
+  //     duration: 1.4,
+  //     ease: "power2.inOut",
+  //   });
 
-    gsap.to(txt, {
-      color: "white",
-      duration: 1.4,
-    });
-  };
+  //   gsap.to(txt, {
+  //     color: "white",
+  //     duration: 1.4,
+  //   });
+  // };
 
   const handleClick = () => {
     tl.restart();
 
     // Tilbakestill etter ferdig animasjon (1.8 + 1 + litt buffer)
-    gsap.delayedCall(10, resetAnimation);
+    // gsap.delayedCall(10, resetAnimation);
   };
 
   btn.addEventListener("click", handleClick);
