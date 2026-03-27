5a19a6be76d73df116726c2e2bdd450c6b85f25f
Stian Karlsen <stiankarlsen709@gmail.com>
2026-03-27 15:42:54 +0100

    ggg

--- components/gsap/buttonAnimation.js
+++ components/gsap/buttonAnimation.js
@@ -5,51 +5,7 @@ gsap.registerPlugin(MotionPathPlugin);
 
 export default function btnAnimation(btn, txt) {
   if (!btn || !txt) return;
-
-  // Lag interpolert path med 50 punkter
-  const path = [
-    { x: 0, y: 0 }, // start
-    { x: 124, y: -124 },
-    { x: 280, y: -234 },
-    { x: 427, y: -332 },
-    { x: 561, y: -415 },
-    { x: 724, y: -474 },
-    { x: 920, y: -500 }, // slutt
-  ];
-
-  // Array til interpolere punkter
-  const smoothPath = [];
-  // Antall steg for hvert punkt
-  // Antall punkter vi ønsker mellom hvert segment
-  const stepsPerSegment = 6; // du kan øke for mer smoothness
-
-  // Gå gjennom hvert segment mellom punktene
-  for (let i = 0; i < path.length - 1; i++) {
-    const p0 = path[i];
-    const p1 = path[i + 1];
-
-    // Lag flere punkter mellom p0 og p1
-    for (let j = 0; j < stepsPerSegment; j++) {
-      const t = j / stepsPerSegment; // 0 → 1
-      const x = p0.x + (p1.x - p0.x) * t; // lineær interpolasjon
-      const y = p0.y + (p1.y - p0.y) * t; // lineær interpolasjon
-      smoothPath.push({ x, y });
-    }
-  }
-
-  // Legg til siste punkt til slutt
-  smoothPath.push(path[path.length - 1]);
-
-  // const steps = 50;
-  // const xMin = xPoints[0];
-  // const xMax = xPoints[xPoints.length - 1];
-  // const stepSize = (xMax - xMin) / steps;
-
-  // for (let i = 0; i <= steps; i++) {
-  //   const x = xMin + i * stepSize;
-  //   const y = interpolateY(x);
-  //   path.push({ x, y });
-  // }
+  //! https://gsap.com/docs/v3/Plugins/MotionPathPlugin/
 
   // Lag timeline
   const tl = gsap.timeline({ paused: true });
@@ -73,7 +29,13 @@ export default function btnAnimation(btn, txt) {
   tl.to(btn, {
     duration: 3.5,
     motionPath: {
-      path: path,
+      path: [
+        { x: 0, y: 0 },
+        { x: 20, y: 0 },
+        { x: 430, y: 250 },
+        { x: 950, y: -550 },
+      ],
+      type: "cubic",
       curviness: 1.5,
       autoRotate: false,
     },
